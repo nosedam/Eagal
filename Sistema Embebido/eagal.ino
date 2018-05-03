ca597074a2462b7c53f70adb0d93f71f878235ac
federan <federan+github@fastmail.com>
2018-05-03 00:57:44 -0300

    Cambio libreria DHT11. Nuevo manejo de LDR

--- Sistema Embebido/eagal.ino
+++ Sistema Embebido/eagal.ino
@@ -2,9 +2,6 @@
 #include <PietteTech_DHT.h>
 
 // This #include statement was automatically added by the Particle IDE.
-#include <Adafruit_DHT.h>
-
-// This #include statement was automatically added by the Particle IDE.
 #include "Pantalla.h"
 
 // This #include statement was automatically added by the Particle IDE.
@@ -12,15 +9,18 @@
 #define DHTTYPE DHT11
 #define DHTPIN 5
 
-DHT dht(DHTPIN, DHTTYPE);
+PietteTech_DHT dht(DHTPIN, DHTTYPE);
 
 int pFotoresistor = A0;
 
-int fFotoresistor = A5; 
+int fFotoresistor = A5;
 
 int vFotoresistor; 
+
 String nivel_luz;
 String nivel_luz_anterior;
+
+int estado = 0;
 
 uint32_t freemem;
 
@@ -32,6 +32,9 @@ String fecha_anterior;
 
 Timer tFechaHora(1000, actualizarFechaHora);
 Timer tTemperaturaHumedad(30000, actualizarTemperaturaHumedad);
+Timer tAlarma(700, alarma);
+
+int cont = 0;
 
 // FIN AREA DE TIMERS
 
@@ -40,12 +43,13 @@ void setup() {
     Particle.variable("fotoresistor", vFotoresistor);
     Particle.variable("nivel-luz", nivel_luz);
     Particle.variable("memoria", freemem);
+    Particle.variable("cont", cont);
 
     Time.zone(-3);
 
     Serial1.begin(9600);
     
-    dht.begin();
+    //dht.begin(); -- Adafruit DHT
 
     pinMode(pFotoresistor,INPUT);  
     pinMode(fFotoresistor,OUTPUT);
@@ -53,10 +57,12 @@ void setup() {
     digitalWrite(fFotoresistor,HIGH);
     
     actualizarFechaHora();
-    actualizarTemperaturaHumedad();
+    //actualizarTemperaturaHumedad();
+    alarma();
     // ACTIVACION DE TIMERS
     tFechaHora.start();
     tTemperaturaHumedad.start();
+    tAlarma.start();
 
 }
 
@@ -65,15 +71,13 @@ void setup() {
 
 void loop() {
 
-
-
 }
 
 void actualizarFechaHora(){
     time_t timenow = Time.now();
     String aux = Time.format(timenow, "%H:%M:%S");
     Pantalla::setTexto("reloj", aux);
-    aux = Time.format(timenow, "%d-%m-%Y");
+    aux = Time.format(timenow, "%d/%m/%Y");
     if (!aux.equals(fecha_anterior)){ // evitar mandar la fecha cada segundo
         Pantalla::setTexto("calendario",aux);
         fecha_anterior = aux;
@@ -89,15 +93,42 @@ void actualizarFechaHora(){
 }
 
 void actualizarTemperaturaHumedad(){
-    float aux = dht.getTempCelcius();
-    if (!isnan(aux)){
-        Particle.publish(String("Temperatura actualizada ")+aux);
-        Pantalla::setTexto("temperatura", String((int)aux));
-        Pantalla::setTexto("humedad", String((int)dht.getHumidity()));
-    } else {
-        Particle.publish("Temperatura no pudo ser leida");
-    }
+    int respuesta = dht.acquireAndWait(1000);   
+    tAlarma.stop();
     freemem = System.freeMemory(); // actualiza la memoria disponible -- eliminar despues
+    switch (respuesta) {
+        case DHTLIB_OK:
+            Particle.publish(String("Temperatura actualizada"));
+            Pantalla::setTexto("temperatura", String((int)dht.getCelsius()));
+            Pantalla::setTexto("humedad", String((int)dht.getHumidity()));
+            break;
+        case DHTLIB_ERROR_CHECKSUM:
+            Particle.publish(String("CHECKSUM"));
+            break;
+        case DHTLIB_ERROR_ISR_TIMEOUT:
+            Particle.publish(String("ISR TIMEOUT"));
+            break;
+        case DHTLIB_ERROR_RESPONSE_TIMEOUT:
+            Particle.publish(String("RESPONSE TIMEOUT"));
+            break;
+        case DHTLIB_ERROR_DATA_TIMEOUT:
+            Particle.publish(String("DATA TIMEOUT"));
+            break;
+        case DHTLIB_ERROR_ACQUIRING:
+            Particle.publish(String("ACQUIRING"));
+            break;
+        case DHTLIB_ERROR_DELTA:
+            Particle.publish(String("DELTA TIME TOO SMALL"));
+            break;
+        case DHTLIB_ERROR_NOTSTARTED:
+            Particle.publish(String("NOT STARTED"));
+            break;
+        default:
+            Particle.publish(String("UNKNOWN"));
+            break;
+    }
 }
 
-
+void alarma(){
+    cont++;
+}
